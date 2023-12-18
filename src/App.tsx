d4bdb44bbbb97a85bb34e9f8a25fbf616993a7b1
Shamonti <67649277+Shamonti@users.noreply.github.com>
2023-12-18 21:16:58 +0600

    Added header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import Signin from './pages/Signin';
+// import Signin from './pages/Signin';
 // import Signup from './pages/Signup';
+import Header from './ui/Header';
 
 function App() {
   return (
     <>
+      <Header />
       <h1>Main Page</h1>
-      <Signin />
+      {/* <Signin /> */}
       {/* <Signup /> */}
     </>
   );
